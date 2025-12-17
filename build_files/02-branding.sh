777d2903d12026ea53eb585d4f4f9a5cff2cadd1
Lani <puntillol@linuxmail.org>
2025-12-17 10:04:43 -0500

    Fix BUG_SUPPORT_URL in branding script

--- build_files/02-branding.sh
+++ build_files/02-branding.sh
@@ -7,7 +7,7 @@ IMAGE_PRETTY_NAME="Iodine"
 IMAGE_LIKE="fedora"
 HOME_URL="https://github.com/laniku/Iodine/"
 SUPPORT_URL="https://github.com/laniku/Iodine/issues"
-BUG_SUPPORT_URL="https://github.com/ecstasy-rh/laniku/issues"
+BUG_SUPPORT_URL="https://github.com//laniku/Iodine/issues"
 CODE_NAME="Forty-Three"
 #VERSION="${VERSION:-00.00000000}"
 IMAGE_NAME=iodine
@@ -22,6 +22,3 @@ sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
 sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\\\"$BUG_SUPPORT_URL\\\"|" /usr/lib/os-release
 sed -i "s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME=\"iodine\"|" /usr/lib/os-release
 sed -i "s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"$CODE_NAME\"|" /usr/lib/os-release
-
-# Set static hostname
-hostnamectl set-hostname --static iodine
