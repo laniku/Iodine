d190219e688ffeca2beb05ac8387e64df3281d2c
Lani <puntillol@linuxmail.org>
2025-12-17 13:10:14 -0500

    Enhance image info script with new variables
    Updated image information and OS release file settings.

--- build_files/00-image-info.sh
+++ build_files/00-image-info.sh
@@ -1,18 +1,62 @@
 #!/usr/bin/env bash
-set -eoux pipefail
+
+echo "::group:: ===$(basename "$0")==="
+
+set -ouex pipefail
+
+IMAGE_PRETTY_NAME="Iodine"
+IMAGE_LIKE="fedora"
+HOME_URL="https://github.com/laniku/Iodine/"
+DOCUMENTATION_URL="https://github.com/laniku/Iodine/"
+SUPPORT_URL="https://github.com/laniku/Iodine/issues"
+BUG_SUPPORT_URL="https://github.com/laniku/Iodine/issues"
+CODE_NAME="Fourty Three"
+VERSION="${VERSION:-00.00000000}"
 
 IMAGE_INFO="/usr/share/ublue-os/image-info.json"
-IMAGE_VENDOR=laniku
-IMAGE_NAME=iodine
-
-mkdir -p "$(dirname "$IMAGE_INFO")"
-if [ ! -f "$IMAGE_INFO" ]; then
-    echo "{}" > "$IMAGE_INFO"
-fi
-
 IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
 
-sed -i 's/\"image-name\": [^,]*/\"image-name\": \"'"$IMAGE_NAME"'\"/' "$IMAGE_INFO" || true
-sed -i 's|\"image-ref\": [^,]*|\"image-ref\": \"'"$IMAGE_REF"'\"|' "$IMAGE_INFO" || true
+# Image Flavor
+image_flavor="main"
 
-sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release || true
+cat >$IMAGE_INFO <<EOF
+{
+  "image-name": "$IMAGE_NAME",
+  "image-flavor": "$image_flavor",
+  "image-vendor": "$IMAGE_VENDOR",
+  "image-ref": "$IMAGE_REF",
+  "image-tag":"$UBLUE_IMAGE_TAG",
+  "base-image-name": "$BASE_IMAGE_NAME",
+  "fedora-version": "$FEDORA_MAJOR_VERSION"
+}
+EOF
+
+# OS Release File
+sed -i "s|^VARIANT_ID=.*|VARIANT_ID=$IMAGE_NAME|" /usr/lib/os-release
+sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"${IMAGE_PRETTY_NAME} (Version: ${VERSION})\"|" /usr/lib/os-release
+sed -i "s|^NAME=.*|NAME=\"$IMAGE_PRETTY_NAME\"|" /usr/lib/os-release
+sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
+sed -i "s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"$DOCUMENTATION_URL\"|" /usr/lib/os-release
+sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
+sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"$BUG_SUPPORT_URL\"|" /usr/lib/os-release
+sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:universal-blue:${IMAGE_PRETTY_NAME,}|" /usr/lib/os-release
+sed -i "s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,}\"|" /usr/lib/os-release
+sed -i "s|^ID=fedora|ID=${IMAGE_PRETTY_NAME,}\nID_LIKE=\"${IMAGE_LIKE}\"|" /usr/lib/os-release
+sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
+sed -i "s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"$CODE_NAME\"|" /usr/lib/os-release
+sed -i "s|^VERSION=.*|VERSION=\"${VERSION} (${BASE_IMAGE_NAME^})\"|" /usr/lib/os-release
+sed -i "s|^OSTREE_VERSION=.*|OSTREE_VERSION=\'${VERSION}\'|" /usr/lib/os-release
+
+if [[ -n "${SHA_HEAD_SHORT:-}" ]]; then
+echo "BUILD_ID=\"$SHA_HEAD_SHORT\"" >>/usr/lib/os-release
+fi
+
+# Added in systemd 249.
+# https://www.freedesktop.org/software/systemd/man/latest/os-release.html#IMAGE_ID=
+echo "IMAGE_ID=\"${IMAGE_NAME}\"" >> /usr/lib/os-release
+echo "IMAGE_VERSION=\"${VERSION}\"" >> /usr/lib/os-release
+
+# Fix issues caused by ID no longer being fedora
+sed -i "s|^EFIDIR=.*|EFIDIR=\"fedora\"|" /usr/sbin/grub2-switch-to-blscfg
+
+echo "::endgroup::"
